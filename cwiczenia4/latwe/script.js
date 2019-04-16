bc0faf72d6151eb8dcd60d7c83ea2cdd00ab6fa1
krisolew <krisolew@wp.pl>
2019-04-16 13:52:27 +0200

    exercise 4 removed

--- cwiczenia4/latwe/script.js
+++
@@ -1,9 +0,0 @@
-const Module = require('./module');
-
-/**first argument form command line */
-var first = parseInt(process.argv[2]);
-/**second argument form command line */
-var second = parseInt(process.argv[3]);
-
-const obiekt = new Module(first, second);
-console.log(obiekt.sum());
